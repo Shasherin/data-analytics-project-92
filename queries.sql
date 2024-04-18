16ce7b0768cb0891bdaae98a26168e44836acf0f
Shasherin <165906767+Shasherin@users.noreply.github.com>
2024-04-18 15:51:07 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-4)
+/*Напишите запрос, который считает общее количество покупателей из таблицы customers*/
 with customers_checked as( --создаю виртуальную таблицу
 	select *
 	from(select *,
@@ -11,13 +11,13 @@ from customers_checked
 ;
 
 
-второй способ проверки уникальности, что нашел:
+/*второй способ проверки уникальности, что нашел:
 select first_name, middle_initial, last_name, age, COUNT(*)  --каждой дублирующейся после группировки строке присваиваем count
 from customers c 
 group by first_name, middle_initial, last_name, age --группируем по уникальности 4х полей: ФИО + возвраст
-having count (*) = 1 --оставляем только первое из каждой группированной строки(2 и последующие в count указывают на дубль)
+having count (*) = 1 --оставляем только первое из каждой группированной строки(2 и последующие в count указывают на дубль)*/
 
-5a) 
+/*отчет с продавцами у которых наибольшая выручка*/
 select 
 concat(first_name,' ', middle_initial, ' ', last_name) as seller, --объединил столбы  в 1 с пробелами между значениями
 count(s.sales_id) as operations,  --посчитал количество операций после группировки
@@ -32,7 +32,7 @@ order by income desc --сортировка по выручке
 limit 10 --10 первых запросил
 ;
 
-5b)
+/*отчет с продавцами, чья выручка ниже средней выручки всех продавцов*/
 with income_by_seller as (select 
 concat(first_name,' ', middle_initial, ' ', last_name) as seller,
 count(s.sales_id) as operations,
@@ -51,8 +51,7 @@ from income_by_seller
 where  average_income < (select floor(avg(average_income)) from income_by_seller) -- сравнил среднюю выручку каждого продавца с общей ср
 order by average_income
 ;
-Или
- 
+/*Или
 with income_by_seller as (select 
 concat(first_name,' ', middle_initial, ' ', last_name) as seller,
 count(s.sales_id) as operations,
@@ -71,9 +70,9 @@ select seller,
 from income_by_seller
 where  average_income < (select floor(sum(income) / sum(count_of_sales)) from income_by_seller) --сравнил с отношением суммы продаж к количеству продаж
 order by average_income
-;
+;*/
 
-5с)
+/*отчет с данными по выручке по каждому продавцу и дню недели*/
 select 
 concat(first_name,' ', middle_initial, ' ', last_name) as seller,
 to_char(sale_date, 'Day') as day_of_week,  --привели формат даты к дню недели
@@ -87,7 +86,7 @@ group by to_char(sale_date, 'Day'), EXTRACT(ISODOW from sale_date), concat(first
 order by EXTRACT(ISODOW from sale_date), seller --сортировка по номеру дня недели, где понедельник - 1, вскр - 7
 ;
 
-6a)
+/*отчет с возрастными группами покупателей*/
 with category_by_age as (select age, --создал вирт.таблицу
 case when age >= 16 and age <= 25 then '16-25' --добавил возрастную категорию для каждого покупателя
 	 when age >= 26 and age <= 40 then '26-40'
@@ -103,7 +102,7 @@ group by age_category --сгруппировал по возрасту
 order by age_category --отсортировал по возрасту 
 ;
 
-6b)
+/*отчет с количеством покупателей и выручкой по месяцам*/
 select 
 	extract (year from sale_date):: text || '-' || --извлекли год, добавили '-'
 	lpad(extract (month from sale_date):: text, 2, '0') as selling_month, /*извлекли месяц + привели к двузначному числу(добавляем слева 0, до 2х символов в номере) для корректной сортировки*/
@@ -116,7 +115,7 @@ group by extract (year from sale_date):: text || '-' || LPAD(extract (month from
 order by extract (year from sale_date):: text || '-' || LPAD(extract (month from sale_date):: text, 2, '0') --отсортировали по YYYY-MM
 ;
 
-или
+/*или
 select 
 	to_char(s.sale_date, 'YYYY-MM') as selling_month, --оставили только 'YYYY-MM' из даты
 	count(distinct s.customer_id) as total_customers,
@@ -126,9 +125,9 @@ inner join products p
 	on s.product_id = p.product_id
 group by to_char(s.sale_date, 'YYYY-MM') 
 order by to_char(s.sale_date, 'YYYY-MM')
-;
+;*/
 
-6c)
+/*отчет  с покупателями первая покупка которых пришлась на время проведения специальных акций*/
 with rn_sales as ( --создаём виртуальную таблицу
 	select *,
 		row_number() OVER(partition by customer_id order by sale_date, sales_id) AS row /*добавляем номер покупки в разрезе id покупателя, сортируя по дате, id покупки, не хватает времени покупки для уверенности, что покупка у покупателя первая(при наличии нескольких в 1 день)*/
